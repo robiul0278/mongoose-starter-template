24ffbaecdc4afe960574ab0561c9406e72d1a095
robiul0278 <robiul0278@gmail.com>
2025-03-01 18:55:16 +0600

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import { Server } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
 import config from './config';
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    
+    server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port} 😍`);
     });
   } catch (error) {
     console.log(error);
@@ -14,3 +18,19 @@ async function main() {
 }
 
 main();
+
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
